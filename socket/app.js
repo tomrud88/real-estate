a2d010edb7f523c5850c91edeb8122436de649a8
tomrud88 <53145969+tomrud88@users.noreply.github.com>
2024-09-23 13:07:47 +0100

    Update app.js port

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
   },
 });
 
@@ -39,5 +39,7 @@ io.on("connection", (socket) => {
 })
 
 
-
-io.listen("4000")
+const PORT = process.env.PORT || 4000;
+io.listen(PORT, () => {
+  console.log('Socket.IO server running on port ${PORT}`);
+})
