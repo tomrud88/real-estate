94ef69d79b41bf59af5352031ada5c1a5cb82234
Tomasz Rudnicki <tomek198821@wp.pl>
2026-08-18 15:08:02 +0100

    Fix CORS issue and Vercel serverless function limit

--- api/app.js
+++ api/app.js
@@ -1,22 +1,22 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
-import authRoute from './routes/auth.route.js';
-import testRoute from './routes/test.route.js';
-import userRoute from './routes/user.route.js';
-import chatRoute from './routes/chat.route.js';
+import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true }))
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -25,7 +25,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-
 app.listen(8800, () => {
-    console.log("Server is running!");
-})
+  console.log("Server is running!");
+});
